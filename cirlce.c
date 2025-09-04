5eacd18901e0a695a0fd2648a3c1dcd973f7237e
secretpirate309-droid <secretpirate309@gmail.com>
2025-09-04 10:28:23 +0530

    Add files via upload

---
+++ cirlce.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//Write a program to calculate the area and circumference of a circle given its radius.
+
+
+int main()
+{
+    int r = 5;
+
+
+    int area = 3.14 * r;
+
+    printf("Area is : %d \n" , area);
+
+    return 0;
+
+
+}
