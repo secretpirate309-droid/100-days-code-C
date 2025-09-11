f6b7ab5800b997739bf4286fcd363e2bca88ec58
secretpirate309-droid <secretpirate309@gmail.com>
2025-09-11 20:58:17 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//Write a program to input a year and check whether it is a leap year or not using conditional statements.
+
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Check if the year is a leap year
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
